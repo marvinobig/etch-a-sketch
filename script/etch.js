381b257d0b75f3e089fde189938aa1bac7cbb0f3
marvin obig <obig.marv@gmail.com>
2022-01-08 16:00:18 +0100

    added functions and comments

--- script/etch.js
+++ script/etch.js
@@ -1,1 +1,21 @@
+function gridGen() {
+/*  Initialise a variable that gets grid size data from form text input.
+    Turn variable data from string to number data type.
+    Use variable data to create columns & rows of the same number. */
+}
 
+function colorChoice() {
+/*  Add an event listener for mouse hover that triggers the function.
+    Initialise a variable that gets color choice from the down menu.
+    Initialise another variable that gets the user's custom color choice from form text input in hex code format.
+    Use variable data to set background color of generated grid by using class name (gridSquares) for the generated grid to target them. */
+};
+
+function colorDisplay() {
+/*  Add an event listener on a button for when it is clicked.
+    When the button is clicked, display the user's color choice on a square. */
+};
+
+function reset() {
+/*  A reset  button can be clicked to erase grid & color choices. */
+};
