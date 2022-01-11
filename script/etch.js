0ff12a6b805d308db2e0dda2a19d9d87b24dd181
marvin obig <obig.marv@gmail.com>
2022-01-11 08:54:38 +0100

    removed color choice function

--- script/etch.js
+++ script/etch.js
@@ -1,50 +1,21 @@
 document.getElementById("generate").addEventListener("click", gridGen);
 
 function gridGen() {
-    let gridParent = document.getElementsByClassName("gridParent");
-    console.log(gridParent);
-
-    if (gridParent !== false) {
-        let gridSize = document.getElementById("gridInput").value; //Initialise a variable that gets grid size data from from text input.
-        let gridNum = Number(gridSize); //Turn variable data from string to number data type.
+    let gridSize = document.getElementById("gridInput").value; //Initialise a variable that gets grid size data from from text input.
+    let gridNum = Number(gridSize); //Turn variable data from string to number data type.
         
-        //Use variable data to create columns & rows of the same number.
-        let gridContainer = document.getElementById("gridDisplay"); 
+    //Use variable data to create columns & rows of the same number.
+    let gridContainer = document.getElementById("gridDisplay"); 
         
-        for (i = 0; i < gridNum; i++) {
-            let gridParent = document.createElement("div");
-            gridParent.classList.add("gridParent");
-            gridContainer.appendChild(gridParent);
+    for (i = 0; i < gridNum; i++) {
+        let gridParent = document.createElement("div");
+        gridParent.classList.add("gridParent");
+        gridContainer.appendChild(gridParent);
     
-            for (j = 0; j < gridNum; j++) {
-                let gridChild = document.createElement("div");
-                gridChild.classList.add("gridChild");
-                gridParent.appendChild(gridChild);
-            };
-        };
-    }else {
-        let gridChild = document.getElementsByClassName("gridChild");
-        gridChild.remove();
-
-        let gridParent = document.getElementsByClassName("gridParent");
-        gridParent.remove();
-
-        let gridSize = document.getElementById("gridInput").value; //Initialise a variable that gets grid size data from from text input.
-        let gridNum = Number(gridSize); //Turn variable data from string to number data type.
-        
-        //Use variable data to create columns & rows of the same number.
-        let gridContainer = document.getElementById("gridDisplay"); 
-        
-        for (i = 0; i < gridNum; i++) {
-            let gridParent = document.createElement("div");
-            gridParent.classList.add("gridParent");
-            gridContainer.appendChild(gridParent);
-    
-            for (j = 0; j < gridNum; j++) {
-                let gridChild = document.createElement("div");
-                gridChild.classList.add("gridChild");
-                gridParent.appendChild(gridChild);
-            };
+        for (j = 0; j < gridNum; j++) {
+            let gridChild = document.createElement("div");
+            gridChild.classList.add("gridChild");
+            gridParent.appendChild(gridChild);
         };
     };
 };
@@ -54,21 +25,11 @@ function gridGen() {
 
 document.getElementById("useColor").addEventListener("click", colorChoice);
 
-function colorChoice() {
-    //Add an event listener for mouse hover that triggers the function.
-    let squareColor = document.getElementsByClassName("gridChild");
-    
+function colorChoice() {    
     //Initialise a variable that gets color choice from the down menu.
-    let choice = document.getElementById("colorOptions").value;
     //Initialise another variable that gets the user's custom color choice from form text input in hex code format.
-    let customColor = document.getElementById("colorHex").value;
-    //Use variable data to set background color of generated grid by using class name (gridSquares) for the generated grid to target them. 
-    squareColor.
-};
-
-function colorDisplay() {
-    //Add an event listener on a button for when it is clicked.
-    //When the button is clicked, display the user's color choice on a square.
+    //Use variable data to set background color of generated grid by using class name (gridChild) for the generated grid to target them. 
+    //Add an event listener for mouse hover that triggers the function.
 };
 
 
@@ -78,11 +39,4 @@ document.getElementById("reset").addEventListener("click", reset);
 
 function reset() {
     //A reset  button can be clicked to erase grid & color choices.
-    let gridParent = document.getElementById("gridParent");
-
-    if (gridParent === null) {
-        return gridGen();
-    }else {
-        gridParent.remove();
-    };
 };
